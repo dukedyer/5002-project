a4541f8dda769beaf087d0d851805c56d6ee6757
Logan Mednick <52183490+lsmednick@users.noreply.github.com>
2021-11-22 21:11:47 -0500

    Delete src/tables directory

--- src/tables/airlines.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `db_design`.`airlines`;
-CREATE TABLE `db_design`.`airlines` (
-  `airline` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`airline`));
-INSERT INTO `db_design`.`airlines` (`airline`) VALUES ('AMERICAN');
-INSERT INTO `db_design`.`airlines` (`airline`) VALUES ('DELTA');
-INSERT INTO `db_design`.`airlines` (`airline`) VALUES ('SOUTHWEST');
-
-
--- src/tables/roles.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `db_design`.`roles`;
-CREATE TABLE `db_design`.`roles` (
-  `role` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`role`));
-INSERT INTO `db_design`.`roles` (`role`) VALUES ('FIGHT ATTENDANT');
-INSERT INTO `db_design`.`roles` (`role`) VALUES ('GATE ATTENDANT');
-INSERT INTO `db_design`.`roles` (`role`) VALUES ('MANAGER');
-INSERT INTO `db_design`.`roles` (`role`) VALUES ('PILOT');
-
