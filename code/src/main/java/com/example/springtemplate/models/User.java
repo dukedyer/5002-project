a6e50e10575d658a723532c1dd7f4904720bec41
Duke Dyer <dukedyer@gmail.com>
2021-12-05 19:40:02 -0500

    user and userRestDAO updates

--- code/src/main/java/com/example/springtemplate/models/User.java
+++ code/src/main/java/com/example/springtemplate/models/User.java
@@ -1,6 +1,7 @@
 package com.example.springtemplate.models;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 @Table(name="users")
@@ -10,32 +11,39 @@ public class User {
     private Integer id;
     private String firstName;
     private String lastName;
-    private String username;
+    private Date dateOfBirth;
+    private String email;
+    private String userName;
     private String password;
-    private String profilePicture;
-    private String handle;
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
+
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
+
     public String getLastName() { return lastName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+
+    public Date getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(Date dateOfBirth) { this.dateOfBirth = dateOfBirth; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getUserName() { return userName; }
+    public void setUserName(String username) { this.userName = username; }
+
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-    public String getProfilePicture() { return profilePicture; }
-    public void setProfilePicture(String profilePicture) { this.profilePicture = profilePicture; }
-    public String getHandle() { return handle; }
-    public void setHandle(String handle) { this.handle = handle; }
 
-    public User(String username, String password, String first_name, String last_name, String profile_picture) {
-        this.username = username;
+    public User(String firstName, String lastName, Date dateOfBirth, String email, String userName, String password ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.userName = userName;
         this.password = password;
-        this.firstName = first_name;
-        this.lastName = last_name;
-        this.profilePicture = profile_picture;
     }
 
     public User() {}
