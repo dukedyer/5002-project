301839b75aab04b80ca4a03d48604ed9203e2637
Logan Mednick <52183490+lsmednick@users.noreply.github.com>
2021-12-13 16:06:35 -0500

    Added descriptions to site's home page.

--- code/src/main/webapp/react/social/home.js
+++ code/src/main/webapp/react/social/home.js
@@ -67,7 +67,41 @@ const Home = () => {
             </div>
             <div style={box}>
                 <p>Descriptions:</p>
-                Will add more here once we do our write ups.
+                <p>Problem: We seek to provide online visibility to the management of an airport.
+                    Airports have
+                    many job functions essential to their operation, Additionally, airports have a
+                    high
+                    volume of travelers that demand accurate, accessible data for their travel
+                    itinerary. Manual tracking of airport employees and passengers is an almost
+                    insurmountable task given the small margin of error that is expected in airport
+                    record keeping.</p>
+                <p>Solution: Our solution creates a platform for accurate, relational record keeping
+                    for of passenger travel and employee job functions within an airport.
+                    Specifically,
+                    our domain model and web app allows for the creation of passengers, tickets, and
+                    planes. Additionally, airport/airplane employees can be given different roles
+                    (flight attendant, gate attendant, etc) and assigned job specific tasks.
+                    Essentially, our solution seeks to manage the functions within an airport and
+                    airplane.</p>
+                <p>User: The typical user would be a DBA or project manager working at the airport. This
+                    person would have the ability to create tickets for passengers as well as assign
+                    specific staff to trips. It is a platform that seeks to enable end-to-end
+                    administration of the airport. If a form of authentication/user profiles were
+                    implemented in the future, then you may see passengers interacting with the web
+                    app to book tickets on specific trips. However, since the web app user profiles
+                    were out of scope for this project, access to the tables and record creation was
+                    left open. Therefore, the ideal user is one that is allowed to access all the
+                    tables and have full visibility into the functions of an airport</p>
+                <p>
+                    Domain Objects: The Plane represents an airplane. Planes have an serial number
+                    and a name associated with them. The passenger's Ticket has a reference to the
+                    Plane that they will board for their trip, as expected with a flight. The Task
+                    is just that, a task, assigned to an employee that is necessary for the
+                    successful completion of a trip. Tasks can be thought of as a checklist that
+                    employees must complete. For example a task a flight attendant may have assigned
+                    to them is to "perform safety announcements", which are done after boarding a
+                    plane, pre-departure.
+                </p>
             </div>
         </div>
     )
